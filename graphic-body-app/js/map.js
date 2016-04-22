4d5eb392bf8fd9ce641c319335aeb5ebabe35c17
ahayn465 <ahayn465@mtroyal.ca>
2016-04-22 17:04:49 -0600

    posterior.json added, both views rendering out to the screen, ability to enter name and age to global varibales, write to file working

--- graphic-body-app/js/map.js
+++ graphic-body-app/js/map.js
@@ -2,8 +2,10 @@
  * Created by Amanda on 2016-02-15.
  */
 
+
+
 var regions = {};
-var allTheRegions = {};
+var allTheRegions = [];
 var touchedRegions = [];
 
 var theSymptom = '';
@@ -11,6 +13,49 @@ var theSeverity = 0;
 
 var fs = require('fs');
 var anterior = JSON.parse(fs.readFileSync('views/anterior.json', 'utf8'));
+var posterior = JSON.parse(fs.readFileSync('views/posterior.json', 'utf8'));
+
+
+(function initialize() {
+
+    renderMap(anterior['regions'], 'anterior-map');
+    renderMap(posterior['regions'], 'posterior-map');
+
+    for( var i in anterior['regions'] ){
+        allTheRegions.push( anterior['regions'][i] );
+    }
+    for( var i in anterior['regions'] ){
+        allTheRegions.push( posterior['regions'][i] );
+    }
+
+    var modal = document.getElementById('symptomModal');
+
+
+    //Event listeners for the window
+
+    // Modal Save: When the user clicks on save, close the modal and update the dermatomes
+    var closeSpan = document.getElementById('closeModal');
+    closeSpan.onclick = function () {
+        modal.style.display = "none";
+        updateDermatomes();
+    }
+
+    // Update user info
+    var name = document.getElementById('name');
+    var age = document.getElementById('age');
+
+    name.addEventListener('change',function(event) {
+        console.log(name.value);
+    });
+
+    age.addEventListener('change',function(event) {
+        console.log(age.value);
+    })
+
+
+})();
+
+
 
 /**
  * Function renderMap
@@ -21,20 +66,18 @@ var anterior = JSON.parse(fs.readFileSync('views/anterior.json', 'utf8'));
  * @param viewId - The id of the div to render the map in
  */
 function renderMap(data, viewId){
-    //set the whole regions array with the given SVG data
-    allTheRegions = data;
 
     var mapContainer = document.getElementById(viewId);
     document.write(mapContainer);
 
-    var map = new Raphael(mapContainer, 500, 700);
+    var map = new Raphael(mapContainer, 350, 700);
     map.setViewBox(0, 0, 900, 1900 );
 
 
     var style = {
         fill: "#D3AF8E",
-        stroke: "white",
-        "stroke-width": 1,
+        stroke: "#D3AF8E",
+        "stroke-width": 0,
         "stroke-linejoin": "round",
         cursor: "pointer",
     };
@@ -43,19 +86,26 @@ function renderMap(data, viewId){
     var hoverStyle = {
         fill: "#27b7c0"
     }
+    var hoverStyle = {
+        fill: "#27b7c0"
+    }
 
    for(var i=0; i < data.length; i++){
+
        regions[i] = map.path(data[i]['path']);
+
        regions[i]['view'] = data[i]['view'];
        regions[i]['region'] = data[i]['region'];
        regions[i]['dermatome'] = data[i]['dermatome'];
        regions[i]['side'] = data[i]['side'];
 
-
        regions[i]['sympt-pain'] = data[i]['sympt-pain'];
        regions[i]['sympt-numbness'] = data[i]['sympt-numbness'];
        regions[i]['sympt-tingling'] = data[i]['sympt-tingling'];
        regions[i]['sympt-weakness'] = data[i]['sympt-weakness'];
+
+       regions[i]['pathid'] = data[i]['path'];
+
 
    }
 
@@ -68,8 +118,9 @@ function renderMap(data, viewId){
             region[0].addEventListener("mouseenter", function() {
                 region.animate(hoverStyle, animationSpeed);
 
-                if(touchedRegions.indexOf(region['dermatome']) === -1){
-                    touchedRegions.push(region['dermatome']);
+                // if the region has not been added to the array already, add it
+                if(touchedRegions.indexOf(region['pathid']) === -1){
+                    touchedRegions.push(region['pathid']);
                 }
 
             }, true);
@@ -84,11 +135,9 @@ function renderMap(data, viewId){
     }
 
     return addEventListeners(viewId);
-
-
 }
 
-renderMap(anterior['regions'], 'anterior-map');
+
 
 
 function addEventListeners (viewId){
@@ -106,62 +155,51 @@ function addEventListeners (viewId){
             clearInterval(mousedownID);
             mousedownID = -1;
 
-            result = gatherSeverityData();
-
-
+            if(touchedRegions.length > 0){
+                gatherSeverityData();
+            }
         }
     }
     function whilemousedown() {
         console.log('mouse is currently down');
-
     }
     //Assign events
     map.addEventListener("mousedown", mousedown);
     map.addEventListener("mouseup", mouseup);
-    //Also clear the interval when user leaves the window with mouse
 
+    //Also clear the interval when user leaves the window with mouse
     map.addEventListener('click', function(){
-        allTheRegions = [];
+
     })
 }
 
 
-function saveData(){
+function gatherUserData(){
+    var modal = document.getElementById('userModal');
 
-
-
-    // kill the modal symptom slider so that it can be initialixed again on the next swipe
-    slider.noUiSlider.destroy();
-
-
-
-    console.log(theSeverity);
-    console.log(theSymptom);
-
-    var result = allTheRegions.filter(function( obj ) {
-            console.log(obj);
-        if(touchedRegions.includes(obj['dermatome'])){
-            console.log(obj['dermatome']);
-
+    modal.style.display = "block";
+    slider = document.getElementById('slider');
+    noUiSlider.create(slider, {
+        start: [1],
+        step: 1,
+        range: {
+            'min': 1,
+            'max': 10
         }
-
     });
-
 }
-
 
 
 function gatherSeverityData() {
 
     //Display the modal, and create the severity slider
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('symptomModal');
     var dropdown = document.getElementById('symptomDropdown');
 
-
     //get the symptom from the dropdown
-
     //initialize it to the default of pain
     theSymptom = dropdown.options[dropdown.selectedIndex].value;
+
     //and update it if it changes
     dropdown.addEventListener('change', function(){
         theSymptom = dropdown.options[dropdown.selectedIndex].value;
@@ -178,17 +216,130 @@ function gatherSeverityData() {
         }
     });
 
-
     var valueInput = document.getElementById('value-input'),
-        valueSpan = document.getElementById('value-span');
+        valueSpan = document.getElementById('severity-span');
 
     slider.noUiSlider.on('update', function () {
-        theSeverity = slider.noUiSlider.get()
-
+        theSeverity = slider.noUiSlider.get();
     });
 
+}
+
+// called when the modal is closed to update the dermatomes in the selected regions
+function updateDermatomes(){
+
+    console.log(theSeverity);
+    console.log(theSymptom);
+
+    //TODO implement a unique identifier that has region, view, and dermatome combiined
+    //console.log('touched');
+    console.log(touchedRegions);
+    //console.log('All');
+    //console.log(allTheRegions);
+
+   touchedRegions.forEach( updateSvgRegions )
+
+    var result = allTheRegions.filter(function( obj ) {
+        var a = touchedRegions.indexOf(obj['dermatome']);
+
+        if(a > -1){
+           console.log(obj['id']);
+        }
+    });
+}
+
+
+function updateSvgRegions(item, index){
+   // console.log(item);
+
+    var lookup = {};
+    for (var i = 0, len = allTheRegions.length; i < len; i++) {
+
+        if(allTheRegions[i]['path'] === item){
+
+            var symptomString = 'sympt-' + theSymptom;
+            allTheRegions[i][symptomString] = theSeverity;
+
+            console.log(allTheRegions[i]);
+        }
+
+        lookup[allTheRegions[i].pathid] = allTheRegions[i];
+    }
+}
+
+
+function resetMaps(){
+
+    allTheRegions.forEach( function(item){
+        item['sympt-pain'] = 0;
+        item['sympt-numbness'] = 0;
+        item['sympt-tingling'] = 0;
+        item['sympt-weakness'] = 0;
+    });
+
+    console.log("Resetting maps");
+    console.log(allTheRegions);
+}
+
+
+function convertArrayOfObjectsToCSV(args) {
+
+    var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+    data = args.data || null;
+    if (data == null || !data.length) {
+        return null;
+    }
+
+    columnDelimiter = args.columnDelimiter || ',';
+    lineDelimiter = args.lineDelimiter || "\n";
+
+    keys = Object.keys(data[0]);
+    var index = keys.indexOf('path');
+
+    if (index > -1) {
+        keys.splice(index, 1);
+    }
+
+    result = '';
+    result += keys.join(columnDelimiter);
+    result += lineDelimiter;
+
+
+    data.forEach(function(item) {
+        ctr = 0;
+        keys.forEach(function(key) {
+            if (ctr > 0) result += columnDelimiter;
+
+            result += item[key];
+            ctr++;
+        });
+        result += lineDelimiter;
+    });
+
+    console.log(result);
+    return result;
 
 }
+
+
+function saveData(){
+
+    console.log('saving to ');
+
+    var args = { columnDelimiter: ',', data: allTheRegions };
+    var result = convertArrayOfObjectsToCSV(args);
+
+    saveToFile('anterior-test.csv', result);
+}
+
+
+
+
+
+
+
+
 
 
 
